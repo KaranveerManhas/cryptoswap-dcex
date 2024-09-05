6169429e9cd637ea0732cb45abe47860e2466501
Karanveer Singh <82330083+KaranveerManhas@users.noreply.github.com>
2024-09-06 02:30:26 +0530

    Fixed bug in authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -54,7 +54,9 @@ const handler = NextAuth({
                             }
                         }
                     }
-                })
+                });
+
+                return true;
             }
             
             return false;
